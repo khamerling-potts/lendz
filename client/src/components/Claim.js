5cb2925ca9ea49af2b78d362eef1f98efcfccfbe
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2024-02-27 14:03:14 -0500

    select button on hover, reformatting claim

--- client/src/components/Claim.js
+++ client/src/components/Claim.js
@@ -1,12 +1,21 @@
-import { ListGroup, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import { ListGroup, Row, Col, Button } from "react-bootstrap";
 
 function Claim({ listing, claim, mine }) {
+  const [hover, setHover] = useState(false);
   return (
-    <ListGroup.Item>
+    <ListGroup.Item
+      className="claim"
+      onMouseOver={(e) => setHover(true)}
+      onMouseLeave={(e) => setHover(false)}
+    >
       <Row>
-        <Col>{claim.user.username}</Col>
-        <Col>{claim.comment}</Col>
-        <Col>{claim.time}</Col>
+        <Col xs={2}>{claim.user.username}</Col>
+        <Col xs={6}>{claim.comment}</Col>
+        <Col xs={4}>
+          {claim.time}
+          {hover ? <Button id="select-claim>">Select</Button> : null}
+        </Col>
       </Row>
     </ListGroup.Item>
   );
